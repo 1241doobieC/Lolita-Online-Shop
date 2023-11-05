a933e74557559323e1944c308e76a38b4c84d17d
許証淵 <chengyuan@xuzhenguandeMBP.home>
2023-11-05 23:57:48 +0800

    fix: change data type for product price

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,18 +4,40 @@ exports.getAddProduct = (req, res, next) => {
     res.render('admin/edit-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
+        editing: false
     });
 }
 
 exports.postAddProduct = (req, res) => {
     let title = req.body.title; //follow with 'name'
     let imageUrl = req.body.imageUrl;
-    let price = req.body.price;
+    let price = Number(req.body.price);
     let description = req.body.description;
     const product = new Product(title, imageUrl, description, price);
     product.save();
     res.redirect('/')
 };
+
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    if(!editMode){
+        //
+    }
+    const proId = req.params.productId;
+    Product.findById(proId, product => {
+        if(!product){
+            return res.redirect('/');
+        }
+        console.log("Product:" , product);
+        res.render('admin/edit-product', {
+            pageTitle: 'Edit Product',
+            path: '/admin/edit-product',
+            editing: editMode,
+            product: product
+        });
+    })
+    
+}
 
 exports.getProducts = (req, res) => {
     Product.fetchAll( products => {
