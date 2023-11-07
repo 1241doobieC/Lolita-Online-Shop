901cdcea2b2d94061c3d5dadcf366c257fa0f99c
許証淵 <chengyuan@xuzhenguandeMBP.home>
2023-11-07 17:48:40 +0800

    feat: add delete function

--- models/cart.js
+++ models/cart.js
@@ -8,10 +8,11 @@ module.exports = class Cart {
         fs.readFile(p, (err, fileContent) => {
             let cart = {products: [], totalPrice: 0};
             if(!err){
-                cart = JSON.parse(fileContent);
-            }
+                cart = JSON.parse(fileContent); //The cart file at minimum should always be: {"products":[],"totalPrice":0}
+                
+            };
             const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
-            const existingProduct = cart.products[existingProductIndex];
+            const existingProduct =  cart.products[existingProductIndex];
             let updatedProduct;
             if(existingProduct){
                 updatedProduct = { ...existingProduct };
@@ -27,6 +28,32 @@ module.exports = class Cart {
                 console.log(err);
             })
         });
-    }
+    };
+    static deleteProduct(id, productPrice){
+        fs.readFile(p, (err, fileContent) => {
+            if(err){
+                return;
+            }
+            const updatedCart = { ...JSON.parse(fileContent) };
+            const product = updatedCart.products.find( prod => prod.id === id);
+            if( !product ){
+                return;
+            }
+            const produxtQty = product.quantity;
+            updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
+            updatedCart.totalPrice -= (productPrice * produxtQty);
 
+            fs.writeFile(p, JSON.stringify(updatedCart), err => {
+                console.log(err);
+            })
+        })
+    };
+
+    static getCart(cb){
+        fs.readFile(p, (err, fileContent) => {
+            const cart = JSON.parse(fileContent);
+            if(err) cb(null)
+            else cb(cart);
+        })
+    }
 }
