e9c634143035b1c846e957e6d09fa5a10bc71023
許証淵 <chengyuan@xuzhenguandeMBP.home>
2023-11-09 16:49:26 +0800

    fix: fix the routing problem

--- routes/shop.js
+++ routes/shop.js
@@ -9,11 +9,11 @@ const router = express.Router();
 router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
-router.get('/products/:productId', shopController.getProduct);
+router.get('/products/:productId([0-9a-fA-F]{24})', shopController.getProduct);
 router.get('/cart', shopController.getCart);
 router.post('/cart', shopController.postCart);
 router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 router.get('/orders', shopController.getOrders);
 router.get('/checkout', shopController.getCheckOut);
-
+ 
 module.exports = router; 
