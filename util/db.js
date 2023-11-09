cfc275480bbec0808fbcbfa0a412fe57c0c44a9a
許証淵 <chengyuan@xuzhenguandeMBP.home>
2023-11-09 16:47:49 +0800

    fix: create db connection

--- util/db.js
+++ util/db.js
@@ -2,16 +2,29 @@ require('dotenv').config();
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
+
 const mongoConnect = callback => {
     MongoClient.connect(process.env.DB_CONNECT)
     .then( client => {
-        console.log('Successfully connected.');
-        callback(client);
+        console.log('DB successfully connected.');
+        _db = client.db('Lolita');
+        callback();
     })
     .catch( err => {
         console.log(err);
+        throw err;
     });
     
 };
 
-module.exports = mongoConnect;
+const getDB = () => {
+    if(_db){
+        return _db;
+    }
+    throw "No DB found.";
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDB = getDB;
