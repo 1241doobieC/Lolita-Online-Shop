9b547e3012aa1667da2a18b6e3ff12d4dfe31040
許証淵 <chengyuan@xuzhenguandeMBP.home>
2023-11-10 02:30:38 +0800

    fix: set create-order router

--- routes/shop.js
+++ routes/shop.js
@@ -13,6 +13,7 @@ router.get('/products/:productId([0-9a-fA-F]{24})', shopController.getProduct);
 router.get('/cart', shopController.getCart);
 router.post('/cart', shopController.postCart);
 router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+router.post('/create-order', shopController.postOrders);
 router.get('/orders', shopController.getOrders);
 router.get('/checkout', shopController.getCheckOut);
  
